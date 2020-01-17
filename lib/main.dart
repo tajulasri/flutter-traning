a700b19c6b7ad1858544409f2a4073d8dd2297d3
expressobyte <tajulasri@codexpert.my>
2020-01-17 16:37:27 +0800

    layout

--- lib/main.dart
+++ lib/main.dart
@@ -22,22 +22,38 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(title: Text("MY App"), leading: Text("Leading")),
       body: Container(
         child: Center(
           child: Column(
             children: <Widget>[
+              Text(
+                "flutter",
+                style: TextStyle(fontSize: 30),
+              ),
+              Text("flutter"),
+              Container(height: 100, color: Colors.green),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Icon(Icons.home, size: 30),
+                  Text("flutter"),
+                  Icon(Icons.info_outline, size: 50),
+                  Column(
+                    children: <Widget>[
+                      Text("flutter"),
+                      Text("flutter"),
+                    ],
+                  )
+                ],
+              ),
               Text("flutter"),
               Text("flutter"),
-              Text("flutter"),
-              Text("flutter"),
-              Text("flutter"),
-              Text("flutter"),
-              Text("flutter"),
-              Text("flutter"),
-              Text("flutter"),
-              Text("flutter"),
-              Text("flutter"),
+              FlutterLogo(size: 200),
+              Text(
+                "flutter",
+                style: TextStyle(fontSize: 30, color: Colors.blue),
+              ),
               Text("flutter"),
             ],
           ),
